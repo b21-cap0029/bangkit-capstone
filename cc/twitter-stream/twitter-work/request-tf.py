eab8582e32017fe9a023b131c5de704c078d08f4
dzakyraffy <71543444+djkampus@users.noreply.github.com>
2021-05-29 01:09:14 +0700

    perbaikan PoC
    sementara PoC sudah diperbaiki dengan menyesuaikan catatan test API denga cURL.
    
    kalau uji dengan curl bisa aja menghasilkan responses. tapi pada saat test API dari luar cloud env gagal menyelesaikan requests.
    file pencatat responses sudah bisa menerima hasilnya dengan test PoC sebelumnya.
    
    perbaikan yang dibuat ada banyak sih
    ikutin aja commentlinenya.semoga membantu

--- cc/twitter-stream/twitter-work/request-tf.py
+++ cc/twitter-stream/twitter-work/request-tf.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import csv
 import os
 import os.path
@@ -6,7 +7,11 @@ import os.path
 print(os.getcwd())
 #workdir = os.chdir("..")#naik satu folder untuk ambil twitter-fetch
 #print(workdir)
-url="localhost"#dummy load untuk setting request tensor
+#url="http://35.247.137.111:8501/v1/models/model:predict"#dummy load untuk setting request tensor 
+test_payload = '{"instances":[["Ded"], ["Meninggol"]]}'
+
+test = requests.post(url,data=test_payload)
+print(test.status_code)
 
 
 #path = "E:\BANGKIT\Project\bangkit-capstone\cc\twitter-stream" ##belum bisa akses relative path sama belum bisa buat workpathnya(aulah)
@@ -18,15 +23,20 @@ with open('twitter_fetch.csv','r') as f:
             line_counter =+ 1
             print("this is first entry")
         else:
-            #create request and catch the responses
-            payload = row[2]
-            #resp = requests.post(url,data=payload)#atur parameter request disini(doc with requests)
-            result = 1#test workload
+            result = 1#test workload !!!Jangan lupa buang line ini kalau sudah bekerja API Requestnya
             #result = resp.responses
             if result >= 0.5:#sementara aja ini kalau resultnya dipasang treshold 0.5 dari 1
                 with open('responses-record.csv','a',newline='') as responses:
                     csvWriter = csv.writer(responses)
-                    csvWriter.writerow([row[0],row[1],result])
-                    print(row[0],row[1],row[2],result)
+                    #create request and catch the responses prepare the requests
+                    payload = {}
+                    payload["instances"] = row[2]
+                    json_payload = json.dumps(payload)
+                    #resp = requests.post(url,json=json_payload)#atur parameter request disini(doc with requests)
+                    #result_dict = resp.json()  #ambil hasilnya dalam bentuk json. cari key response(hasil test dengan curl
+                    #hasilnya result_dict["predictions"]
+                    #result = result_dict["predictions"]  #untuk mempermudah akses nilai
+                    print(row[0],row[1],row[2],result)#verbose all record
+                    #csvWriter.writerow([row[0],row[1],result]) #opsi dengan line ini hanya menyimpan id_str,username, dan prediction score
     
 
