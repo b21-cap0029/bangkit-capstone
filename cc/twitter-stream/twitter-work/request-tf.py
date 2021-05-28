688eabcd597315484c2549781c22b2ac63fd7e0c
dzakyraffy <71543444+djkampus@users.noreply.github.com>
2021-05-28 11:18:25 +0700

    streamer sudah siap
    tapi ini masih dalam bentuk csv. kalau mau streamlined ke tensor dan simpan yang prediction tinggi aja mungkin lain cerita
    
    oiya sekaligus nggak tau kenapa kalau pencarian dengan language indonesia only hasilnya nggak ada(mungkin karena feednya sedikit kali ya? siapa tau) misalnya nggak sepakat bisa aja diubah dengan location
    
    Next Workload prediction_request

--- cc/twitter-stream/twitter-work/request-tf.py
+++ cc/twitter-stream/twitter-work/request-tf.py
@@ -1,4 +1,19 @@
 import requests
+import csv
 
-#url='link to ai platform'
-#payload=
+url="localhost"#dummy load
+
+with ('twitter-fetch','r') as f:
+    csvReader= csv.reader(f)
+    
+
+payload="dummy"#dummyload
+resp = requests.post(url,data=payload)
+
+
+
+resp_dict = resp.json()
+print(resp_dict["prediction"])
+
+with open('prediction_res', 'a') as f:
+    csv.writer#dummy load
--- cc/twitter-stream/twitter-work/streamer.py
+++ cc/twitter-stream/twitter-work/streamer.py
@@ -1,10 +1,10 @@
 #prepare for dependencies from tweepy
 
 
-from token import CONSUMER_KEY
-from token import CONSUMER_SECRET
-from token import ACCESS_TOKEN_KEY as ACCESS_TOKEN
-from token import ACCESS_TOKEN_SECRET
+from API_KEY import CONSUMER_KEY
+from API_KEY import CONSUMER_SECRET
+from API_KEY import ACCESS_TOKEN
+from API_KEY import ACCESS_TOKEN_SECRET
 import tweepy
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -17,7 +17,8 @@ import csv
 ##set boundary entries
 language = ['id']
 #coordinates = []
-
+#filesave ='twitter_fetch'+(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))+'.csv'
+filesave = 'twitter_fetch.csv'
 #authentication to twitter
 auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
@@ -33,13 +34,10 @@ except:
 
 #setup listener
 class Listener(StreamListener):
-    reset_block_counter = 100
-    #to avoid loop of unlimited retries connection
-    #set to reconnect trial to n
 
+    #def on_status(self, status):
+     #   print(status.text)
 
-    def on_status(self, status):
-        print(status)
 
     def on_error(self, status_code):
         if status_code !=420:
@@ -50,40 +48,36 @@ class Listener(StreamListener):
 
     
     def on_data(self, data):
-        self.data_payload =+ 1
-        tweet = json.loads(data)
-        username = tweet.get("user",{}).encode('utf-8').strip()
-        text = tweet.get("text","").encode('utf-8').strip()
-        _id = tweet.get("user",{}).get("id","")
-        #print username
+        json_tweet = json.loads(data)
+        tweet = {'id':json_tweet['user']['id_str'],'screenName':json_tweet['user']['screen_name'],'text:':json_tweet['text']}
+        id = json_tweet['user']['id_str']
+        name = json_tweet['user']['screen_name']
+        text_tweet = json_tweet['text']
+        print(text_tweet)
+        with open(filesave,'a', newline='') as f:
+            csvWriter = csv.writer(f)
+            csvWriter.writerow([id,name,text_tweet])
+
 
 
 
 #creating the stream
 customListener = Listener()
-customStream = Stream(auth = api.auth, listener = customListener)
+customStream = Stream(auth, listener = customListener)
+
+#membuat filesave
+#BEWARE karena disini pakai mode W potensi ilang, ubah modenya untuk siap PROD
+with open (filesave, 'w', newline='') as csvFile:
+    csvWriter = csv.writer(csvFile)
+    csvWriter.writerow(['ID_STR', 'USERNAME','TWEET_TEXT'])
 
 #start streaming
-try:
-    customStream.filter(languages=['id'])#batas bahasa indonesia
-    #adding file csv
-    #filesave ='twitter_fetch'+(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))+'.csv'
-    filesave = 'twitter_fetch.csv'
-    #storing the tweet fetched
-    with open (filesave, 'a+', newline='') as csvFile:
-        csvWriter = csv.writer(csvFile)
-        for tweet in tweepy.Cursor(customStream).items():
-            tweets_encoded = tweet.text.encode('utf-8')
-            tweet_decoded = tweets_encoded.decode('utf-8')
-            csvWriter.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), tweet.user, tweet.author.name , tweet_decoded,tweet._json["user"]["location"]])
-except KeyboardInterrupt as e:
-    print("stopped")
-finally:
-    print('done')
-    customStream.disconnect()
-
+#customStream.sample(languages=["id"])
+customStream.filter(track=['Trump'],languages=["en","fr","es","id"])#batas bahasa indonesia wajib pakai track(APIKEYnya tidak dapat akses feed semua tweet)
+#pengerjaan parameter filternya disini
 
 #preparing for prediction request
+
 
 #catch the prediction responses
 
