0997d3cf2dd86f3e99d37ffbf1c8de567d7701b9
giovanism <giovanmail@gmail.com>
2021-06-03 19:51:11 +0700

    Remove created_date and owner_id from aaida-access's payload

--- cc/twitter-stream/twitter-work/aaida-access.py
+++ cc/twitter-stream/twitter-work/aaida-access.py
@@ -19,7 +19,6 @@ with open ('responses-record.csv','r') as record:#tidak ada kewajiban menulis fi
         print(line)
         now = datetime.now()#input waktu created
         calendar = now.isoformat()
-        payload["created_date"] = calendar
         payload["tweet_id"] = int(line[2])# index[2] menyimpan tweet_id
         if float(line[4]) >= treshold:#index[4] menyimpan score prediction
             payload["class"] = "Teridentifikasi"
@@ -27,7 +26,6 @@ with open ('responses-record.csv','r') as record:#tidak ada kewajiban menulis fi
             payload["class"] = "tidak teridentifikasi"
         payload["score"] = float(line[4])#index[4]menimpan score prediction
         payload["twitter_user_id"] = int(line[0])#menyimpan id twitter user
-        payload["owner_id"] = 0 #dummy values
         payload["is_claimed"] = False
         payload["is_closed"] = False        
         print(dict(payload)) #just test
