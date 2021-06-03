6fe6634a2370a3444f7a874608899b5706efe83d
dzakyraffy <71543444+djkampus@users.noreply.github.com>
2021-06-03 20:51:44 +0700

    post_process the streamer output
    untuk menghasilkan input yang lebih bersih di request-tf

--- cc/twitter-stream/twitter-work/streamer.py
+++ cc/twitter-stream/twitter-work/streamer.py
@@ -12,8 +12,42 @@ from API_KEY import (ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY,
                      CONSUMER_SECRET)
 from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
+import re
 
 
+
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"
+        u"\U0001F700-\U0001F77F"  # alchemical symbols
+        u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+        u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+        u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+        u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+        u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+        u"\U00002702-\U000027B0"  # Dingbats
+        u"\U000024C2-\U0001F251"# flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
+
+def pre_process(text):
+    # remove links
+    text = re.sub("https://t.co/\S*","",text) 
+
+    # remove newline
+    text = re.sub("\n","",text)
+
+    #  remove digits
+    text = re.sub("[0-9]","",text)
+
+    # remove emojis
+    text = deEmojify(text)
+
+    return text
 ##set boundary entries
 language = ['id']
 #coordinates = []
@@ -49,11 +83,12 @@ class Listener(StreamListener):
     
     def on_data(self, data):
         json_tweet = json.loads(data)
-        print(json_tweet)
+        #print(json_tweet)
         id_user = json_tweet['user']['id']
         id_tweet = json_tweet['id']
         name = json_tweet['user']['screen_name']
         text_tweet = json_tweet['text']
+        text_tweet = pre_process(text_tweet)
         print(text_tweet)
         with open(filesave,'a', newline='') as f:
             csvWriter = csv.writer(f)
